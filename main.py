5d89c2d254e853c9f593d298a084856bce6d6f82
semimatt123 <clemematthieu@gmail.com>
2026-02-16 13:59:04 +0100

    test.matthieu.stage2

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Bonjour Doctolib")
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Mon serveur FastAPI fonctionne 🚀"}
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    return {"user_id": user_id}
+
